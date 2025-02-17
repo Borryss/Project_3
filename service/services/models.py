20a1c63280419047e0b9ee5f0536d98bce0e723b
Borryss <115550381+Borryss@users.noreply.github.com>
2025-02-17 18:29:54 +0200

    part 6

--- service/services/models.py
+++ service/services/models.py
@@ -2,6 +2,8 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 from clients.models import Client
+from services.tasks import set_price
+
 
 
 # Create your models here.
@@ -9,6 +11,17 @@ from clients.models import Client
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+    def save(self, *args, **kwargs):
+
+        if self.full_price != self.__full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Service: {self.name} , Price: {self.full_price}"
@@ -24,6 +37,17 @@ class Plan(models.Model):
     plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
     discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+    def save(self, *args, **kwargs):
+
+        if self.discount_percent != self.__discount_percent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
+
     def __str__(self):
         return f"Plan: {self.plan_type} , Discount: {self.discount_percent}"
 
@@ -32,6 +56,9 @@ class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+    price = models.PositiveIntegerField(default=0)
+
+
 
     def __str__(self):
         return f"Subscription: {self.plan}  ;  {self.service}  ;  {self.client} "
