1561f12a00f82eeca2894050782c6e3ac84a8c5e
Borryss <115550381+Borryss@users.noreply.github.com>
2025-02-18 20:33:58 +0200

    part 7

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task
 from celery_singleton import Singleton
+from django.db import transaction
 from django.db.models import F
 
 
@@ -7,12 +8,13 @@ from django.db.models import F
 def set_price(subscription_id):
     from services.models import Subscription
 
-    subscription = Subscription.objects.filter(id=subscription_id).annotate(
-        annotated_price=F('service__full_price') -
-        F('service__full_price') * F('plan__discount_percent') / 100.00).first()
+    with transaction.atomic():
+        subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(
+            annotated_price=F('service__full_price') -
+            F('service__full_price') * F('plan__discount_percent') / 100.00).first()
 
-    subscription.price = subscription.annotated_price
-    subscription.save()
+        subscription.price = subscription.annotated_price
+        subscription.save()
 
 
 
