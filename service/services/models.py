24a67dee98c163711bcf928b784b72fb87278032
Borryss <115550381+Borryss@users.noreply.github.com>
2025-02-22 18:28:10 +0200

    part 9

--- service/services/models.py
+++ service/services/models.py
@@ -58,7 +58,7 @@ class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
-    price = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0, db_index=True)
 
     def save(self, *args, **kwargs):
 
